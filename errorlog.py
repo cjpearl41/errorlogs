6d5782741cc3c4efd142092adefa8bc6e255b688
cjpearl41 <43967923+cjpearl41@users.noreply.github.com>
2020-03-29 23:58:52 -0600

    Add files via upload

---
+++ errorlog.py
@@ -0,0 +1,101 @@
+def readfile():
+    fo=open("/home/shivam/Documents/test.txt","r")
+    lines=fo.read().split("\n")
+    fo.close()
+    str="System.err:"
+    words=[]
+    st=[]
+    li=[]
+    s=[]
+    comp=[]
+    count=0
+    l={}
+    trace=dict()
+    temp=""
+    y=""
+    component=[]
+    count=0
+    #A for loop for checking if there is an error in lines
+    for i in range(0,len(lines)):
+        if str in lines[i]:
+        #Checks if index lines is in words, and if not, it adds it
+            if lines[i] not in words:
+                words.append(lines[i].split())
+    for i in words:
+        pid=i[2]
+        tid=i[3]
+        print("pid=",pid, )
+        print("tid=",tid, ) 
+        print("i[6]=",i[6], )
+        #Checks for an exception in words based on i and prints it
+        if "Exception" in i[6]:
+            ex=i[6]
+            print("ex=",ex, )
+            del li[:]
+            if len(s)!=0:
+                print("\n")
+                print("Stack trace")
+                for x in s:
+                    for k in x:
+                        print(k, )
+                    print()
+                print()
+            print("\n")
+            li=i[6].split(".")
+            print(li[2])
+            #Adds the exception to s if it isn't already
+            if i not in s:
+                s.append(i)
+            del li[:]
+            del s[:]
+            #Prints the error details
+        else:
+            if i not in s:
+                s.append(i)
+            if len(s)>2:
+                continue
+            li=i[7].split(".")
+            print("i[7]",i[7])
+            print("li=",li)
+            x=consplit(li)
+            print("\nComponent:",x)
+            temp=x
+            #Prints the different information of where the error is
+            print("temp=",temp, )      
+            print("File:",li[-3],".java")
+            print("Function:",li[-2],"()")
+            for x in range(0,len(li[-2])):
+                if li[-2][x]=="(":
+                    break
+                else:
+                    y=""+li[-2][x]
+                    print(y, )
+            print("()")
+            print("Line number: ", )
+            for x in range(0,len(li[-1])-1):
+                if li[-1][x].isdigit():
+                    print(li[-1][x], )
+            print()
+            #Counts the number of errors
+            count=count+1
+        if ex in l.keys():
+            l.update({ex:count})
+        else:
+            l.update({ex:1})
+    print("\nErrors:\n")
+    for i,j in l.items():
+        if "Exception" in i:
+            if ":" in l.keys():
+                print(i, )
+            else:
+                print(i,":")
+            print(j)
+            print()
+        
+def consplit(x):
+    y=""
+    for i in range(0,3):
+        y=y+""+x[i]+"."
+    return y
+
+readfile()
